fd66d05c597ae42c96558919aaf378f50f317b0c
leah <Your Emailmuzhgan.bakhtyar7@gmail.com>
2026-06-17 13:42:11 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,12 @@
+
+full_name = "Muzhgan Bakhtyar"
+nickname = "Leah"
+age = 15
+use_python_before = False
+hobbies = ["painting", "sweing", "poetry"]
+favorites = {
+    "movies": "Spiderman",
+    "food": "Mantu",
+    "hobby": "Painting"
+    }
+    print(full_name, age, hobbies)
